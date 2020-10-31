a436baed768546c2b7d9481dcaaf7781bb6f65c6
TooboT <w0rk.we6+web@gmail.com>
2020-11-01 00:38:30 +0900

    (heroku) edit:wsgi

--- djpj/wsgi.py
+++ djpj/wsgi.py
@@ -2,8 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpj.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpj.settings.dev') #edit:
-#🔗https://stackoverflow.com/questions/24488891/gunicorn-errors-haltserver-haltserver-worker-failed-to-boot-3-django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpj.settings')
 
 application = get_wsgi_application()

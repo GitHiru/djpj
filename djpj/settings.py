20d48a4b1b48d5be654d8f50c2861a8e2e19e2dc
TooboT <w0rk.we6+web@gmail.com>
2020-11-01 16:57:14 +0900

    (heroku) edit DEBUG=False

--- djpj/settings.py
+++ djpj/settings.py
@@ -3,7 +3,7 @@ import os #add
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True #edit
+DEBUG = False #edit
 
 ALLOWED_HOSTS = ['*'] #edit
 
@@ -121,8 +121,7 @@ except ImportError:
 if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 #add:AWS S3 ローカルでもS3使用
 from djpj.aws.conf import *

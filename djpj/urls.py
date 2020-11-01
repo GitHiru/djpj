d222c77a7d01bf02a389c1bcdc6e83b50383aae0
TooboT <w0rk.we6+web@gmail.com>
2020-11-01 17:29:41 +0900

    (heroku) edit urls media

--- djpj/urls.py
+++ djpj/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
 if settings.DEBUG: # =True
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    from django.urls import re_path
-    from django.views.static import serve
-    urlpatterns += re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # from django.urls import re_path
+    # from django.views.static import serve
+    # urlpatterns += re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})

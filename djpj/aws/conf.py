33da12705e0583581bd2935c3f464e267273f148
TooboT <w0rk.we6+web@gmail.com>
2020-11-03 17:18:47 +0900

    (heroku) remove:collectstatic edit:settings

--- djpj/aws/conf.py
+++ djpj/aws/conf.py
@@ -1,14 +1,15 @@
 import os
 
 # add: AWS S3
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'djpj.aws.utils.MediaStorage'
+
 AWS_ACCESS_KEY_ID       = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY   = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION         = 'media'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'djpj.aws.utils.MediaStorage'
 MEDIA_URL            = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 AWS_S3_FILE_OVERWRITE = False
